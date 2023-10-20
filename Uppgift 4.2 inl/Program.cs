9fc63c4a40ae427d9c0ef3ee2d5c82278822f1bd
Urkagurka <143172181+Urkagurka@users.noreply.github.com>
2023-10-20 13:06:41 +0200

    Add project files.

---
+++ Uppgift 4.2 inl/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Uppgift_4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string svar2;
+            bool slut = false;
+            int stor = 0;
+            int tal;
+
+           
+            while (slut == false)
+            {
+                try
+                {
+                    Console.WriteLine("Vill du skriva in ett till heltal?");
+                    svar2 = Console.ReadLine();
+                    if (svar2 == "J")
+                    {
+                        Console.WriteLine("Skriv ett heltal");
+                        tal = int.Parse(Console.ReadLine());
+                        if (tal > stor)
+                        {
+                            stor = tal;
+                        }
+                    }
+                    
+                    if (svar2 == "N")
+                    {
+                        Console.WriteLine("Ditt svar är " + stor);
+                        slut = true;
+                        Console.ReadKey();
+                    } 
+
+                }
+                catch 
+                { 
+                    Console.WriteLine("FEL");
+                }
+
+
+
+            }
+            
+
+
+        }
+    }
+
+
+}
